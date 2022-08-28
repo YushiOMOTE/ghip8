8165887368938318f3c2321f4d7b794fa7e3d4aa
YushiOMOTE <yushiomote@gmail.com>
2022-08-28 11:15:58 +0900

    Migrate to ebiten

--- go.mod
+++ go.mod
@@ -2,16 +2,15 @@ module example/hello
 
 go 1.19
 
-require github.com/faiface/pixel v0.10.0
+require github.com/hajimehoshi/ebiten/v2 v2.3.8
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
-	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
-	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
-	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/stretchr/testify v1.7.2 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/jezek/xgb v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
+	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image/color"
+	"log"
 	"os"
 	"time"
 
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
-	"github.com/faiface/pixel/pixelgl"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
+
+var Terminated = errors.New("terminated")
+
+var KeyMap = [...]ebiten.Key{
+	ebiten.KeyX,
+	ebiten.Key1,
+	ebiten.Key2,
+	ebiten.Key3,
+	ebiten.KeyQ,
+	ebiten.KeyW,
+	ebiten.KeyE,
+	ebiten.KeyA,
+	ebiten.KeyS,
+	ebiten.KeyD,
+	ebiten.KeyZ,
+	ebiten.KeyC,
+	ebiten.Key4,
+	ebiten.KeyR,
+	ebiten.KeyF,
+	ebiten.KeyV,
+}
+
+type Game struct {
+	cpu CPU
+}
+
+func (g *Game) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return Terminated
+	}
+
+	for i, k := range KeyMap {
+		if inpututil.IsKeyJustPressed(k) {
+			g.cpu.Keyboard.Press(uint8(i))
+		}
+		if inpututil.IsKeyJustReleased(k) {
+			g.cpu.Keyboard.Release(uint8(i))
+		}
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for x := 0; x < g.cpu.Vram.Width; x++ {
+		for y := 0; y < g.cpu.Vram.Height; y++ {
+			if g.cpu.Vram.Get(x, y) {
+				screen.Set(x, y, color.White)
+			} else {
+				screen.Set(x, y, color.Black)
+			}
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return g.cpu.Vram.Width, g.cpu.Vram.Height
+}
 
 func main() {
 	romfile := os.Args[1]
@@ -28,45 +87,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Loaded %d bytes\n", count)
+	fmt.Printf("Loaded %d bytes from %s\n", count, romfile)
 
 	cpu := NewCPU(rom)
-
-	pixelgl.Run(func() {
-		run(&cpu)
-	})
-}
-
-var KeyMap = [...]pixelgl.Button{
-	pixelgl.KeyX,
-	pixelgl.Key1,
-	pixelgl.Key2,
-	pixelgl.Key3,
-	pixelgl.KeyQ,
-	pixelgl.KeyW,
-	pixelgl.KeyE,
-	pixelgl.KeyA,
-	pixelgl.KeyS,
-	pixelgl.KeyD,
-	pixelgl.KeyZ,
-	pixelgl.KeyC,
-	pixelgl.Key4,
-	pixelgl.KeyR,
-	pixelgl.KeyF,
-	pixelgl.KeyV,
-}
-
-func run(cpu *CPU) {
-	cfg := pixelgl.WindowConfig{
-		Title:  "Chip-8",
-		Bounds: pixel.R(0, 0, 640, 320),
-		VSync:  true,
-	}
-
-	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
 
 	go func() {
 		for range time.Tick(time.Millisecond) {
@@ -74,34 +97,15 @@ func run(cpu *CPU) {
 		}
 	}()
 
-	for !win.Closed() && !win.Pressed(pixelgl.KeyEscape) {
-		for i, k := range KeyMap {
-			if win.JustPressed(k) {
-				cpu.Keyboard.Press(uint8(i))
-			}
-			if win.JustReleased(k) {
-				cpu.Keyboard.Release(uint8(i))
-			}
+	ebiten.SetWindowSize(640, 320)
+	ebiten.SetWindowTitle("Chip-8")
+
+	if err := ebiten.RunGame(&Game{cpu}); err != nil {
+		switch err {
+		case Terminated:
+			fmt.Printf("Terminated")
+		default:
+			log.Fatal(err)
 		}
-
-		scale := 10.0
-		imd := imdraw.New(nil)
-		for x := 0; x < cpu.Vram.Width; x++ {
-			for y := 0; y < cpu.Vram.Height; y++ {
-				if cpu.Vram.Get(x, y) {
-					imd.Color = color.White
-				} else {
-					imd.Color = color.Black
-				}
-
-				px := float64(x) * scale
-				py := float64((cpu.Vram.Height - y - 1)) * scale
-				imd.Push(pixel.V(px, py), pixel.V(px+scale, py+scale))
-				imd.Rectangle(0.0)
-			}
-		}
-		imd.Draw(win)
-
-		win.Update()
 	}
 }
